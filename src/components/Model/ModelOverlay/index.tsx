a498aa08b2617c7ea35ae4c6a33b3c8ffa282c4b
camilaferreiranas <camila_fn97@hotmail.com>
2021-10-10 17:43:34 -0300

    Hook de scroll

--- src/components/Model/ModelOverlay/index.tsx
+++ src/components/Model/ModelOverlay/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react'; 
+import useWrapperScroll from '../useWraperScroll';
 import {Container} from './styles';
 
 const ModelOverlay : React.FC = ({children}) => {
+    const {scrollY} =  useWrapperScroll();
+   
     return (
         <Container>
            {children}
